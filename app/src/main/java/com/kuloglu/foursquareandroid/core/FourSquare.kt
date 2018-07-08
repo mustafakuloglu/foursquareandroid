01f54a4973fbe72445f89fe6c2d7e72187251e5b
mustafa.kuloglu <mustafa.kuloglu@generalmobile.com>
2018-07-08 12:51:01 +0300

    get token

---
+++ app/src/main/java/com/kuloglu/foursquareandroid/core/FourSquare.kt
@@ -0,0 +1,6 @@
+package com.kuloglu.foursquareandroid.core
+
+object FourSquare {
+    const val CLIENT_ID = "RMGPUOON505FAXPPDDV5QEKRWFKFKD1EES0XVLBWA50DSRN1"
+    const val CLIENT_SECRET = "NE5LPMC55KXDMGMJDSP45NZNMZMAETQURDGBAMSO100XMM04"
+}
--- app/src/main/java/com/kuloglu/foursquareandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/kuloglu/foursquareandroid/ui/main/MainActivity.kt
@@ -1,15 +1,46 @@
 package com.kuloglu.foursquareandroid.ui.main
 
-import android.arch.lifecycle.ViewModelProviders
-import android.databinding.DataBindingUtil
+import android.content.Intent
 import android.os.Bundle
+import com.foursquare.android.nativeoauth.FoursquareOAuth
+import com.foursquare.android.nativeoauth.model.AccessTokenResponse
+import com.foursquare.android.nativeoauth.model.AuthCodeResponse
 import com.kuloglu.foursquareandroid.R
 import com.kuloglu.foursquareandroid.core.BaseActivity
+import com.kuloglu.foursquareandroid.core.FourSquare
 import com.kuloglu.foursquareandroid.databinding.ActivityMainBinding
 
+
 class MainActivity : BaseActivity<MainActivityViewModel, ActivityMainBinding>(MainActivityViewModel::class.java) {
+
+    private val REQUEST_CODE_FSQ_CONNECT = 1111
+    private val REQUEST_CODE_FSQ_TOKEN_EXCHANGE = 2222
+    lateinit var codeResponse: AuthCodeResponse
+    lateinit var tokenResponse: AccessTokenResponse
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = FoursquareOAuth.getConnectIntent(this, FourSquare.CLIENT_ID)
+        startActivityForResult(intent, REQUEST_CODE_FSQ_CONNECT)
+    }
+
+
     override fun initViewModel(viewModel: MainActivityViewModel) {
         binding.viewModel = viewModel
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        when (requestCode) {
+            REQUEST_CODE_FSQ_CONNECT -> {
+                codeResponse = FoursquareOAuth.getAuthCodeFromResult(resultCode, data)
+                val intent = FoursquareOAuth.getTokenExchangeIntent(this, FourSquare.CLIENT_ID, FourSquare.CLIENT_SECRET, codeResponse.code)
+                startActivityForResult(intent, REQUEST_CODE_FSQ_TOKEN_EXCHANGE)
+            }
+            REQUEST_CODE_FSQ_TOKEN_EXCHANGE -> {
+                tokenResponse = FoursquareOAuth.getTokenFromResult(resultCode, data)
+            }
+        }
+
     }
 
     override fun getLayoutRes() = R.layout.activity_main
