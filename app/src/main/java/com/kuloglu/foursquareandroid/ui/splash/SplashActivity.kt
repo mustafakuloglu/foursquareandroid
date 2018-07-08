e89c523cf88b6bb38f49463c1906f775352650fb
mustafa.kuloglu <mustafa.kuloglu@generalmobile.com>
2018-07-08 11:59:04 +0300

    splash edit

--- app/src/main/java/com/kuloglu/foursquareandroid/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/kuloglu/foursquareandroid/ui/splash/SplashActivity.kt
@@ -12,7 +12,6 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
         startThread()
     }
 
